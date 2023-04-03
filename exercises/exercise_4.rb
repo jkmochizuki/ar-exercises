ea980716b84d3778c42b6ff4dd3e3f60e46419d0
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-03 13:37:48 -0700

    Create and fetch stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,24 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# 1
+Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+# 2
+@mens_stores = Store.where(mens_apparel: true)
+
+# 3
+@mens_stores.each do |element|
+  puts "Name: #{element.name}"
+  puts "Annual Revenue: #{element.annual_revenue}"
+end
+
+# 4
+@womens_store = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+
+@womens_store.each do |element|
+  puts "Name: #{element.name}"
+  puts "Annual Revenue: #{element.annual_revenue}"
+end
