e1a8570ddff62c203461d8e456b157c59f8b9f7f
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-03 14:37:44 -0700

    Implement validation to models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,26 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# 1
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, :store_id, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+# 2
+puts "Please enter a store name" 
+store_name = gets.chomp
+
+# 3
+@new_store = Store.create(name: store_name)
+
+# 4
+if @new_store.invalid?
+  puts "Failed to save store"
+  puts @new_store.errors.full_messages
+end
