2d027f861d600ec0f4e4535f6d633e29a769437d
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-03 13:12:00 -0700

    Assign stores to instance variables

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,8 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+store_1 = Store.find_by(id: 1)
+@store1 = store_1
+
+store_2 = Store.find_by(id: 2)
+@store2 = store_2
